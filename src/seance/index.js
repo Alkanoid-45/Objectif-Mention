4f9edf517ec641e08554108b1c350c5de76a2dc8
Admin <clgorau@stpbb.org>
2024-06-22 22:19:02 +0200

    h

--- src/seance/index.js
+++ src/seance/index.js
@@ -38,7 +38,11 @@ function ajouterSeance(){
 
 function afficherSeances() {
     var seanceContainer = document.getElementById("seanceContainer");
-    seanceContainer.innerHTML = "a";
+    try {
+        seanceContainer.innerHTML = "";
+        } catch (e) {
+        console.log(e);
+    }
 
     for (var i = 0; i < listeSeances.length; i++) {
         var seance = listeSeances[i];
@@ -64,7 +68,11 @@ function afficherSeances() {
 
         nouvelleSeance.appendChild(nombreInscrits);
         nouvelleSeance.appendChild(boutonInscription);
-        seanceContainer.appendChild(nouvelleSeance);
+        try {
+            seanceContainer.appendChild(nouvelleSeance);
+        } catch (e) {
+            console.log(e);
+        }
     }
     
     console.log(listeSeances);
@@ -271,59 +279,3 @@ function isValidEmail(email) {
     console.log(email)
     return email.includes('@stpbb.org');
 }
-
-// Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
-import "firebase/analytics"
-import "firebase/auth"
-import "firebase/app"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAkedT0XedGHTUhxcJjcdToYsx4JZuWnRw",
-  authDomain: "objectif-mention-17a48.firebaseapp.com",
-  databaseURL: "https://objectif-mention-17a48-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "objectif-mention-17a48",
-  storageBucket: "objectif-mention-17a48.appspot.com",
-  messagingSenderId: "575509147435",
-  appId: "1:575509147435:web:4d75f80a5cac090adadf6e",
-  measurementId: "G-2NNPF733H1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
-
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-
-
-  export default firebase;
