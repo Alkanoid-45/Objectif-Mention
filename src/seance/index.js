61fbb68653b96a826c74455c65529f0bd54033c9
Macario73 <unconfigured@null.spigotmc.org>
2024-06-22 12:54:46 +0200

    m

--- src/seance/index.js
+++ src/seance/index.js
@@ -38,7 +38,7 @@ function ajouterSeance(){
 
 function afficherSeances() {
     var seanceContainer = document.getElementById("seanceContainer");
-    seanceContainer.innerHTML = "a";
+    seanceContainer.innerHTML = "";
 
     for (var i = 0; i < listeSeances.length; i++) {
         var seance = listeSeances[i];
@@ -181,7 +181,7 @@ function validerInscription() {
 
     if (!isValidEmail(email)) {
         document.getElementById("emailError").textContent = "Veuillez entrer un e-mail valide.";
-        alert("Veuillez entrer un e-mail valide."); 
+        alert("Please enter a valid email.");
         return;
     } else {
         document.getElementById("emailError").textContent = "";
@@ -271,59 +271,3 @@ function isValidEmail(email) {
     console.log(email)
     return email.includes('@stpbb.org');
 }
-
-// Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
-import "firebase/analytics"
-import "firebase/auth"
-import "firebase/app"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAkedT0XedGHTUhxcJjcdToYsx4JZuWnRw",
-  authDomain: "objectif-mention-17a48.firebaseapp.com",
-  databaseURL: "https://objectif-mention-17a48-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "objectif-mention-17a48",
-  storageBucket: "objectif-mention-17a48.appspot.com",
-  messagingSenderId: "575509147435",
-  appId: "1:575509147435:web:4d75f80a5cac090adadf6e",
-  measurementId: "G-2NNPF733H1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
-
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-  signOut(auth).then(() => {
-    // Sign-out successful.
-  }).catch((error) => {
-    // An error happened.
-  });
-
-
-  export default firebase;
